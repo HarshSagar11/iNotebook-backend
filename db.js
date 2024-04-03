ebfb246ed8e960b792d7662bf387450cd6df64de
HarshSagar11 <harshsagarkumar1111@gmail.com>
2024-04-03 18:44:43 +0530

    initial commit

---
+++ db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const mongooseURI = 'mongodb://localhost:27017/';
+
+const connectToMongo = () =>{
+    mongoose.connect(mongooseURI)
+    .then(()=>{
+        console.log('connection successful')
+    }).catch(()=>{
+        console.log('connectionn - unsuccessful');
+    })
+};
+
+module.exports = connectToMongo;
